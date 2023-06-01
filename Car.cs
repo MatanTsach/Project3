0ec6a6f824fb8f8fbab7f170d7d37dd6ac9d4930
Matan Tsach <matan.tsach@gmail.com>
2023-06-01 16:20:27 +0300

    alpha 0.4

--- Car.cs
+++ Car.cs
@@ -1,10 +1,22 @@
 public class Car : Vehicle
 {
     protected string m_CarColor = "";
-    protected int m_DoorsAmount;
+    protected int m_Doors;
 
     public Car() : base()
     {
         m_VehicleFields.Add("Color");
+    }
+
+    public string Color
+    {
+        get { return m_CarColor; }
+        set { m_CarColor = value; }
+    }
+
+    public int Doors
+    {
+        get { return m_Doors; }
+        set { m_Doors = value; }
     }
 }
---
+++ ConsoleUI.cs
@@ -0,0 +1,16 @@
+using System.Text;
+
+public class ConsoleUI
+{
+
+    private void showMenu()
+    {
+        StringBuilder sb = new StringBuilder();
+        sb.Append("------- MENU -------");
+    }
+    public static void Main()
+    {
+        Console.WriteLine("Welcome to Matan and Dolev's Garage").
+
+    }
+}
